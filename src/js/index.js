86d6feca4c496c3606a3f55c4d79db67592c2ce8
ivy <original541029@gmail.com>
2018-12-17 14:29:48 +0800

    webpack prototype

---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+const arr = ['mike', 'andy', '111', '123'];
+arr.map(obj => console.log(obj))
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  }
+};
